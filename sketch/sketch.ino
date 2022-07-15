f3aac32684f7ec39c381e9b5ec2b5c737d702e6d
neolao <contact@neolao.com>
2022-07-15 13:58:04 +0200

    up

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,52 +2,106 @@
 #include <PN532_I2C.h>
 #include <PN532.h>
 #include <NfcAdapter.h>
+#include <SoftwareSerial.h>
+#include <DFRobotDFPlayerMini.h>
+
+// NFC reader with I2C
 PN532_I2C pn532_i2c(Wire);
 NfcAdapter nfc = NfcAdapter(pn532_i2c);
 
+// MP3 Player
+#define PLAYER_RX_PIN 12
+#define PLAYER_TX_PIN 11
+uint8_t volume = 15;
+uint8_t fileCount = 0;
+uint8_t fileIndex = 0;
+SoftwareSerial playerSerial(PLAYER_RX_PIN, PLAYER_TX_PIN);
+DFRobotDFPlayerMini player; 
+
 void setup(void) {
+  setupPlayer();
+
   Serial.begin(115200);
-  Serial.println("System initialized");
+
+  Serial.println("Initialization");
+  initialize();
+  Serial.println("Initialized");
+  
   nfc.begin();
 }
 
-void loop() {
-  readNFC();
+void setupPlayer() {
+  playerSerial.begin(9600);
+  player.begin(playerSerial);
+  player.setTimeOut(500);
+  player.reset();
+  player.disableDAC();
+  player.volume(volume); 
 }
 
-void readNFC() {
-  if (nfc.tagPresent()) {
-    NfcTag tag = nfc.read();
-
-    if (tag.hasNdefMessage()) {
-      NdefMessage message = tag.getNdefMessage();
-      int recordCount = message.getRecordCount();
-      for (int i = 0; i < recordCount; i++) {
-        NdefRecord record = message.getRecord(i);
-
-        Serial.print("  TNF: ");
-        Serial.println(record.getTnf());
-        Serial.print("  Type: ");
-        Serial.println(record.getType()); // will be "" for TNF_EMPTY
-
-        // The TNF and Type should be used to determine how your application processes the payload
-        // There's no generic processing for the payload, it's returned as a byte[]
-        int payloadLength = record.getPayloadLength();
-        byte payload[payloadLength];
-        record.getPayload(payload);
-        Serial.print("  Payload (HEX): ");
-        PrintHexChar(payload, payloadLength);
-
-        // Skip characters because the payload contains headers
-        int skipCharacterCount = (int) payload[0] + 1;
-        String payloadAsString = "";
-        for (int characterIndex = skipCharacterCount; characterIndex < payloadLength; characterIndex++) {
-          payloadAsString += (char)payload[characterIndex];
-        }
-        Serial.print("  Payload (as String): ");
-        Serial.println(payloadAsString);
-      }
-    }
+void initialize() {
+  while(fileCount < 2) {
+    fileCount = player.readFileCounts();
   }
+}
+
+String command = "";
+void loop() {
+  command = readCommandFromNFC();
+  if (command == "") {
+    return;
+  }
+  
+  Serial.print("Command: ");
+  Serial.println(command);
+
+  fileIndex = command.toInt();
+  Serial.print("File index: ");
+  Serial.println(fileIndex);
+
   delay(500);
+}
+
+String readCommandFromNFC() {
+  if (!nfc.tagPresent()) {
+    return "";
+  }
+  
+  NfcTag tag = nfc.read();
+
+  if (!tag.hasNdefMessage()) {
+    return "";
+  }
+
+  
+  NdefMessage message = tag.getNdefMessage();
+  int recordCount = message.getRecordCount();
+  for (int i = 0; i < recordCount; i++) {
+    NdefRecord record = message.getRecord(i);
+
+    /*
+    Serial.print("  TNF: ");
+    Serial.println(record.getTnf());
+    Serial.print("  Type: ");
+    Serial.println(record.getType()); // will be "" for TNF_EMPTY
+    */
+
+    // The TNF and Type should be used to determine how your application processes the payload
+    // There's no generic processing for the payload, it's returned as a byte[]
+    int payloadLength = record.getPayloadLength();
+    byte payload[payloadLength];
+    record.getPayload(payload);
+    //Serial.print("  Payload (HEX): ");
+    //PrintHexChar(payload, payloadLength);
+
+    // Skip characters because the payload contains headers
+    int skipCharacterCount = (int) payload[0] + 1;
+    String payloadAsString = "";
+    for (int characterIndex = skipCharacterCount; characterIndex < payloadLength; characterIndex++) {
+      payloadAsString += (char)payload[characterIndex];
+    }
+    //Serial.print("  Payload (as String): ");
+    //Serial.println(payloadAsString);
+    return payloadAsString;
+  }
 }
